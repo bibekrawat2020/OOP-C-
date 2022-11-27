1bc43b8154a5c9ceff3693fa740d3f82cdd73f15
Bibek Rawat <bibekrawat14@gmail.com>
2022-11-27 20:19:12 +0545

    Created Constructors with Parameters

--- Program.cs
+++ Program.cs
@@ -1,14 +1,20 @@
-﻿Car.ford();
+﻿Car.myMethod();
 class Car
 {
-    string? model;
-    public Car()
+    string model;
+    string color;
+    int year;
+    public Car(string modelName, string modelColor, int modelYear)
     {
-        model = "Mustang";
+        model = modelName;
+        color = modelColor;
+        year = modelYear;
     }
-    public static void ford()
+    public static void myMethod()
     {
-        Car myObj = new Car();
-        Console.WriteLine(myObj.model);
+        Car myObj1 = new Car("Mustang", "Red", 1969);
+        Car myObj2 = new Car("Astra", "White", 2005);
+        Console.WriteLine(myObj1.model);
+        Console.WriteLine(myObj2.model);
     }
 }
