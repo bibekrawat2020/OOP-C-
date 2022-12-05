90b09eb472f3e2d4ebbb2d25ee8367728e75732d
Bibek Rawat <bibekrawat14@gmail.com>
2022-12-05 19:06:57 +0545

    Implemented struct

--- Program.cs
+++ Program.cs
@@ -1,37 +1,16 @@
-﻿SaleItem.MyResult();
-public class SaleItem
+﻿TestStruct.MyFormula();
+public struct Rectangle
 {
-    string _name;
-    decimal _cost;
-
-    public SaleItem(string name, decimal cost)
+    public int Width, Height;
+}
+public class TestStruct
+{
+    public static void MyFormula()
     {
-        _name = name;
-        _cost = cost;
-    }
-
-    public string Name
-    {
-        get => _name;
-        set => _name = value;
-    }
-
-    public decimal Price
-    {
-        get => _cost;
-        set => _cost = value;
-    }
-
-    public static void MyResult()
-    {
-        SaleItem[] items = new SaleItem[3];
-        items[0] = new SaleItem("Apple", 1.99m);
-        items[1] = new SaleItem("Orange", 2.99m);
-        items[2] = new SaleItem("Banana", 3.99m);
-
-        foreach (SaleItem item in items)
-        {
-            Console.WriteLine(item.Name + " " + item.Price);
-        }
+        Rectangle r = new Rectangle();
+        r.Width = 10;
+        r.Height = 20;
+        Console.WriteLine("Width = {0}, Height = {1}", r.Width, r.Height);
+        Console.WriteLine("Area = {0}", r.Width * r.Height);
     }
 }
